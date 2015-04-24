28374669f116ccd9f7b9901ada9afed74faff953
Dicko Sow <s12.dsow@gmail.com>
2015-04-24 10:08:41 -0400

    set up devise, rspec, capybara, factory girl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
