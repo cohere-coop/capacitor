74fa5297ee132a708c0c17e23dce19b879f11951
Wimsy113 <Krystyna.d.l.ewing@gmail.com>
2015-04-29 18:10:13 -0400

    changes to project file

--- app/models/capacity.rb
+++ app/models/capacity.rb
@@ -1,9 +1,22 @@
-class Capacity < ActiveRecord::Base
+class Capacity < ActiveRecord::Migration
 
-   def total_time
+  def create_capacity
+    create_table :capacity do |t|
+      t.string :project_id
+      t.string :account_id
+      t.string :client
+      t.string :project
+      t.string :time
+      t.string :url
 
-   end
 
-   def created_at
+      t.timestamps null: false
+    end
   end
+end
+
+def add_capacity
+  create_table do |c|
+
+
 end
