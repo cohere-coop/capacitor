f335bf6f4f477ea0411750a1929c31c47c21cf7e
Dicko Sow <s12.dsow@gmail.com>
2015-04-30 23:11:42 -0400

    merged capacity and project branches

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,8 @@ class Account < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :capacities
+  has_many :projects, through: :capacities
+  has_many :owned_projects, foreign_key: :client_id
 end
