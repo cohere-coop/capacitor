9be8df48abfe7c44dc0438aaaa5bdb857fe55c6e
Dicko Sow <s12.dsow@gmail.com>
2015-05-07 00:07:18 -0400

    Fixing indentation and spaces

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :account, aliases: [:client, :worker] do
-  	sequence(:email) { |i| "account-#{i}@example.com" }
-  	password "password"
+    sequence(:email) { |i| "account-#{i}@example.com" }
+    password "password"
   end
 end
--- spec/features/worker_picks_project_spec.rb
+++ spec/features/worker_picks_project_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 feature 'worker picks project' do
-	Given!(:current_account) { FactoryGirl.create(:account) }
-	Given!(:project_to_work_on) { FactoryGirl.create(:project) }
-	Given!(:project_not_to_work_on) { FactoryGirl.create(:project) }
-	# TODO extract this to a helper
-	When { visit new_account_session_path
+  Given!(:current_account) { FactoryGirl.create(:account) }
+  Given!(:project_to_work_on) { FactoryGirl.create(:project) }
+  Given!(:project_not_to_work_on) { FactoryGirl.create(:project) }
+  # TODO extract this to a helper
+  When { visit new_account_session_path
     fill_in 'Email', with: current_account.email
     fill_in 'Password', with: current_account.password
     click_button 'Log in'}
