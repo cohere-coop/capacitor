59e222db30dc0e15f2f621f0fca7b9621c03d148
Dicko Sow <s12.dsow@gmail.com>
2015-05-08 15:13:18 -0400

    Added line breaks in project helper

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,5 +1,13 @@
 module ProjectHelper
  def options_for_project_amount
-    options_for_select([['1 eighth-day', 1], ['1 quarter-day', 2],['3 eighth-days', 3], ['1 half-day',4], ['5 eighth-days', 5], ['3 quarter-days', 6],['7 eight-days', 7],['1 whole day',8]])
+  options_for_select([
+    ['1 eighth-day', 1],
+    ['1 quarter-day', 2],
+    ['3 eighth-days', 3],
+    ['1 half-day',4],
+    ['5 eighth-days', 5],
+    ['3 quarter-days', 6],
+    ['7 eight-days', 7],
+    ['1 whole day',8]])
   end
 end
