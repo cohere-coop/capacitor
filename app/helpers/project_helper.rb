5cb13395bb329bd406332fad7121ebc1e9ec141e
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-05-08 15:04:09 -0500

    A checkbox is a more friendly way to mark DNB
      - Moved some magic numbers to constants
      - Switched to the checkbox for DNB

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,13 +1,23 @@
 module ProjectHelper
- def options_for_project_amount
-  options_for_select([
+  AMOUNT_OPTIONS = [
     ['1 eighth-day', 1],
     ['1 quarter-day', 2],
     ['3 eighth-days', 3],
-    ['1 half-day',4],
+    ['1 half-day', 4],
     ['5 eighth-days', 5],
     ['3 quarter-days', 6],
     ['7 eight-days', 7],
-    ['1 whole day',8]])
+    ['1 whole day', 8]
+  ]
+
+  WORST = 1
+  BAD = 2
+  OKAY = 3
+  GOOD = 4
+  BEST = 5
+  QUALITY_OPTIONS = [WORST, BAD, OKAY, GOOD, BEST]
+
+  def options_for_project_amount
+    options_for_select(AMOUNT_OPTIONS)
   end
 end
