f1fdc12716886268cbcc92294aa04092df72d754
Dicko Sow <s12.dsow@gmail.com>
2015-05-10 22:07:11 -0400

    Created capacity_remaining method so that user can see the remaining capacity for a project.

--- app/models/capacity.rb
+++ app/models/capacity.rb
@@ -3,4 +3,18 @@ class Capacity < ActiveRecord::Base
   belongs_to :worker, class_name: "Account"
 
   validates :amount, :quality, :worked_at, :do_not_bill, presence: true
+
+  after_create :update_capacity_remaining
+
+
+  def update_capacity_remaining
+    self.project.capacity_remaining = self.project.total_capacity_owed
+
+    capacity_used = self.project.capacity_used += self.amount
+
+    capacity_remaining = self.project.capacity_remaining -= capacity_used
+
+    self.project.update_attributes(capacity_remaining: capacity_remaining, capacity_used: capacity_used)
+  end
+
 end
