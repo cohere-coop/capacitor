4774e4bd2e869ea4f162bc362f10ca5a85798d46
Dicko Sow <s12.dsow@gmail.com>
2015-05-10 22:07:11 -0400

    Created capacity_remaining method so that user can see the remaining capacity for a project.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508134515) do
+ActiveRecord::Schema.define(version: 20150510040128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,8 +53,11 @@ ActiveRecord::Schema.define(version: 20150508134515) do
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.integer  "client_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.integer  "total_capacity_owed"
+    t.integer  "capacity_used",       default: 0
+    t.integer  "capacity_remaining",  default: 0
   end
 
   add_index "projects", ["client_id"], name: "index_projects_on_client_id", using: :btree
