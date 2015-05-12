2f2e5489fb9c54793bfef49a9523afbc243f6c50
Wimsy113 <Krystyna.d.l.ewing@gmail.com>
2015-05-11 20:47:01 -0400

    Fixed failing tests due to order

--- spec/features/logging_time_spec.rb
+++ spec/features/logging_time_spec.rb
@@ -17,9 +17,9 @@ feature 'Logging time' do
   When { select("4", from: "Quality") }
   When { fill_in("Worked at", with: "2015-05-06") }
 
-  context "when tracking time as not do not bill" do
+  context "when tracking time as billable" do
     When { click_link_or_button "Log Capacity" }
-    Then { expect(Capacity.find_by(worker: current_account, amount: 4, quality: 4, worked_at: "2015-05-06", do_not_bill: true, project: project_to_work_on)).to be_present }
+    Then { expect(Capacity.find_by(worker: current_account, amount: 4, quality: 4, worked_at: "2015-05-06", do_not_bill: false, project: project_to_work_on)).to be_present }
   end
 
   context "when tracking time as do not bill" do
