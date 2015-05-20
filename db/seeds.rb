f7fceaf1429559cd7c171512c5e1bd24ad15ddc4
Dicko Sow <s12.dsow@gmail.com>
2015-05-19 21:07:54 -0400

    Cleaning out seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,3 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-unless Rails.env.production?
-
-  client = Account.create_with(password: "password", name: "Client", username: "client").find_or_create_by(email: "client@example.com")
-  worker = Account.create_with(password: "password", name: "Worker", username: "worker").find_or_create_by(email: "worker@example.com")
-
-  arpa = client.owned_projects.find_or_create_by(name: "ARPANet", total_capacity_owed: 10)
-end
