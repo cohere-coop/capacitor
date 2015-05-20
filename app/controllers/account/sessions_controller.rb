d16fe01d3b2b7dd4c9781d34aa16432189520170
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-05-20 08:49:26 -0500

    Rubocop doesn't like not-nesting

--- app/controllers/account/sessions_controller.rb
+++ app/controllers/account/sessions_controller.rb
@@ -1,8 +1,10 @@
-class Account::SessionsController < Devise::SessionsController
-  # POST /resource/sign_in
-  def create
-    super do
-      track_event("Account logged in")
+class Account
+  class SessionsController < Devise::SessionsController
+    # POST /resource/sign_in
+    def create
+      super do
+        track_event("Account logged in")
+      end
     end
   end
 end
