789f33ca99be20e7f3916515f3cbcb6d1b048079
Dicko Sow <s12.dsow@gmail.com>
2015-05-19 21:26:21 -0400

    Making rubocop happy with changes to migration files

--- db/migrate/20150512191409_add_uuid_to_account.rb
+++ db/migrate/20150512191409_add_uuid_to_account.rb
@@ -1,6 +1,6 @@
 class AddUuidToAccount < ActiveRecord::Migration
   def change
-    enable_extension 'uuid-ossp'
+    enable_extension "uuid-ossp"
     add_column :accounts, :uuid, :uuid, default: "uuid_generate_v4()", null: false
   end
 end
