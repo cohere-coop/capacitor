5ce4bab1e4af656723276a7507f12022693ba2a1
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-05-20 22:59:45 -0500

    The puma config was very messy

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,11 @@
-workers Integer(ENV["WEB_CONCURRENCY"] || 2)
-threads Integer(ENV["MIN_THREADS"] || 5), Integer(ENV["MAX_THREADS"] || 5)
+workers(Integer(ENV["WEB_CONCURRENCY"]) || 2)
+threads(Integer(ENV["MIN_THREADS"]) || 5, Integer(ENV["MAX_THREADS"]) || 5)
 
 preload_app!
 
-rackup      DefaultRackup
-port        Integer(ENV["PORT"]) || 3000
-environment ENV["RACK_ENV"] || "development"
+rackup(DefaultRackup)
+port(Integer(ENV["PORT"]) || 3000)
+environment(ENV["RACK_ENV"] || "development")
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
