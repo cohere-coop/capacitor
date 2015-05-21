a81d86ced0b7a2c9579e789ce29fcda7fe053d25
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-05-20 21:18:12 -0500

    This test had a good goal, but went about it oddly

--- spec/features/logging_time_spec.rb
+++ spec/features/logging_time_spec.rb
@@ -3,37 +3,27 @@ require "rails_helper"
 feature "Logging time" do
   include_context "account login"
 
-  Given!(:project_to_work_on) { FactoryGirl.create(:project) }
-  Given!(:project_not_to_work_on) { FactoryGirl.create(:project) }
+  let(:worked_at) { "2015-05-06" }
+  let(:log) { Log.find_by(worked_at: worked_at) }
 
-  When { click_link_or_button project_to_work_on.name }
+  Given!(:project) { FactoryGirl.create(:project) }
+
+  When { click_link_or_button project.name }
 
   When { select("1 half-day", from: "Amount") }
   When { select("4", from: "Quality") }
-  When { fill_in("Worked at", with: "2015-05-06") }
+  When { fill_in("Worked at", with: worked_at) }
 
   context "when tracking time as billable" do
-    When { click_link_or_button "Log Capacity" }
-    Then do
-      expect(Log.find_by(account: current_account,
-                              amount: 4,
-                              quality: 4,
-                              worked_at: "2015-05-06",
-                              do_not_bill: false,
-                              project: project_to_work_on)).to be_present
-    end
+    When { click_link_or_button("Log Capacity") }
+    Then { expect(log.do_not_bill).to be(false) }
+    Then { expect(log).to be_persisted }
   end
 
   context "when tracking time as do not bill" do
     When { check("Do Not Bill") }
-    When { click_link_or_button "Log Capacity" }
-    Then do
-      expect(Log.find_by(account: current_account,
-                              amount: 4,
-                              quality: 4,
-                              worked_at: "2015-05-06",
-                              do_not_bill: true,
-                              project: project_to_work_on)).to be_present
-    end
+    When { click_link_or_button("Log Capacity") }
+    Then { expect(log.do_not_bill).to be(true) }
+    Then { expect(log).to be_persisted }
   end
 end
