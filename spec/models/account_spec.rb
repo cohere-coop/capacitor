b3a2da83b5e6eada8791b10f5db09e4adb6ea636
Wimsy113 <Krystyna.d.l.ewing@gmail.com>
2015-05-21 14:59:20 -0400

    update spec to raise error on name validation

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,7 +2,10 @@ require "rails_helper"
 
 RSpec.describe Account, type: :model do
   let(:account) do
-    FactoryGirl.create(:account)
+    FactoryGirl.create(:account, name: name)
+  end
+  let(:name) do
+    "Casper"
   end
   it "auto-generates a uuid at the database level" do
     # We reload to pull the fields back from the DB since
@@ -13,8 +16,11 @@ RSpec.describe Account, type: :model do
   end
   context "validations" do
     describe "name" do
+      let(:name) do
+        ""
+      end
       it "detects lack of name" do
-        expect(account).to_not be_valid
+        expect{account}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
       end
     end
   end
