e480b8845718f438e92fe10c347b3a8f82442ee5
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-05-21 19:18:08 -0500

    Automatically order by name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-
 case Rails.env
 when "production"
   if Account.count.zero? && Project.count.zero?
