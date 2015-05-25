7c7472a1d10bcabb19571f4207002bb2304278f9
Dicko Sow <s12.dsow@gmail.com>
2015-05-24 23:38:49 -0400

    Worker can see projects with infinite capacities clearly labeled

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -1,6 +1,13 @@
 class ProjectDecorator < Draper::Decorator
   delegate_all
 
+  def infinite_capacity?
+    if object.capacity == -1
+      "Capacity Remaining: Infinite"
+    else
+      "Capacity Remaining: #{project.capacity_remaining}"
+    end
+  end
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
   #
---
+++ spec/features/project_decorator_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe ProjectDecorator do
+  let(:project) { instance_double(Project) }
+  #  Rubocop error: Spec/DescribedClass: Use described_class instead of ProjectDecorator
+  # let(:decorator) { ProjectDecorator.new(project) }
+  let(:decorator) { described_class.new(project) }
+
+  before do
+    allow(project).to receive(:id).and_return(1)
+  end
+
+  describe "#infinite_capacity?" do
+    let(:infinite_capacity) { decorator.infinite_capacity? }
+
+    context "when capacity is -1" do
+      let(:project) { instance_double(Project, capacity: -1) }
+      it "returns infinite capacity when capacity is -1" do
+        expect(infinite_capacity).to eq("Capacity Remaining: Infinite")
+      end
+    end
+
+    context "when capacity is > 0" do
+      let(:project) { FactoryGirl.create(:project) }
+      it "returns the remaining value of capacity left" do
+        # allow(project).to receive(:capacity_remaining).and_return(capacity - capacity_logged)
+        expect(infinite_capacity).to eq("Capacity Remaining: #{project.capacity_remaining}")
+      end
+    end
+  end
+end
