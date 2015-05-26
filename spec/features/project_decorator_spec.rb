998b4c7bbaec80615a038b3856ee5dee1849e5e4
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2015-05-25 22:35:06 -0400

    changed capacity remaining infinite to just infinity

--- spec/features/project_decorator_spec.rb
+++ spec/features/project_decorator_spec.rb
@@ -10,20 +10,19 @@ describe ProjectDecorator do
     allow(project).to receive(:id).and_return(1)
   end
 
-  describe "#infinite_capacity?" do
+  describe "#capacity_remaining" do
     let(:infinite_capacity) { decorator.capacity_remaining }
 
     context "when capacity is -1" do
       let(:project) { instance_double(Project, capacity: -1) }
       it "returns infinite capacity when capacity is -1" do
-        expect(infinite_capacity).to eq("Capacity Remaining: Infinite")
+        expect(infinite_capacity).to eq("Infinity")
       end
     end
 
     context "when capacity is > 0" do
       let(:project) { FactoryGirl.create(:project) }
       it "returns the remaining value of capacity left" do
-        # allow(project).to receive(:capacity_remaining).and_return(capacity - capacity_logged)
         expect(infinite_capacity).to eq(project.capacity_remaining)
       end
     end
