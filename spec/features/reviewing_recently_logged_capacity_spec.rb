6d6065c38ce11cc23e18f515ca19e6ae8ff86990
Zee Spencer <zee@zeespencer.com>
2015-05-29 13:46:36 -0700

    Test each part of the summary independently

--- spec/features/reviewing_recently_logged_capacity_spec.rb
+++ spec/features/reviewing_recently_logged_capacity_spec.rb
@@ -4,6 +4,8 @@ feature "Reviewing logged capacity" do
   Given!(:recent_log) { FactoryGirl.create(:recent_log, account: current_account).decorate }
   Given!(:old_log) { FactoryGirl.create(:old_log, account: current_account).decorate }
 
-  Then { within("##{recent_log.dom_id}") { expect(page).to have_content(recent_log.summary) } }
+  Then { within("##{recent_log.dom_id}") { expect(page).to have_content(recent_log.worked_at) } }
+  Then { within("##{recent_log.dom_id}") { expect(page).to have_content(recent_log.project.name) } }
+  Then { within("##{recent_log.dom_id}") { expect(page).to have_content(recent_log.billable?) } }
   Then { expect(page).not_to have_selector("#{old_log.dom_id}") }
 end
