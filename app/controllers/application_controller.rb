47e83c80a7bbe7b368c8cced41fc4ffe6660a7a4
Zee Spencer <zee@zeespencer.com>
2015-05-30 14:05:03 -0700

    Use segment_rails 0.2.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
 
   include SegmentRails
 
+  def user_identifier
+    current_account ? current_account.id : nil
+  end
+
   alias_method :devise_current_account, :current_account
+
   def current_account
     devise_current_account.present? ? devise_current_account.decorate : nil
   end
