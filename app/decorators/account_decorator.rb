cc9e15291f0260ebea4a95cb8767d0c730c1c340
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2015-05-30 22:35:15 -0400

    created weekly remaining capacity for decorator to return the current remaining capacity for the account

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -4,4 +4,14 @@ class AccountDecorator < Draper::Decorator
   def recent_logs
     logs.recent.decorate
   end
+
+  def weekly_expected_capacity
+    logs.pluck(:amount).sum
+    account.weekly_expected_capacity
+  end
+
+  def remaining_personal_capacity
+    logs.pluck(:amount).sum
+    account.weekly_expected_capacity
+  end
 end
