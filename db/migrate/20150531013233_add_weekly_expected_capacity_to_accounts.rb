f8e61960b38288c686b751cbaed358427a3f16bd
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2015-06-02 20:20:03 -0400

    changed default for weekly_expected_capacity to 0

--- db/migrate/20150531013233_add_weekly_expected_capacity_to_accounts.rb
+++ db/migrate/20150531013233_add_weekly_expected_capacity_to_accounts.rb
@@ -1,5 +1,5 @@
 class AddWeeklyExpectedCapacityToAccounts < ActiveRecord::Migration
   def change
-    add_column :accounts, :weekly_expected_capacity, :integer
+    add_column :accounts, :weekly_expected_capacity, :integer, defalut: 0, null: false
   end
 end
