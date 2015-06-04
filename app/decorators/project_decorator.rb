bac07e195f2178e0807d5e01eb9e2cc45224029a
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-06-04 16:14:08 -0500

    Attempting to better word the cards

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -4,23 +4,67 @@ class ProjectDecorator < Draper::Decorator
   delegate_all
   using CapacityConverter
 
-  def capacity_remaining
-    if project.capacity == -1
-      "Infinity"
+  def per_weekly
+    if project.weekly_burn_rate.zero?
+      "no expected work"
     else
-      project.capacity_remaining.to_business_days
+      "#{weekly_capacity_remaining} capacity remaining this week and #{weekly_burn_rate} per week expected"
     end
   end
 
-  def weekly_burn_rate
-    project.weekly_burn_rate ? project.weekly_burn_rate.to_business_days : "No days"
+  def capacity_remaining
+    if project.capacity_remaining == Float::INFINITY
+      "infinite work remaining"
+    elsif project.capacity_remaining < 0
+      "no more work left"
+    else
+      "#{capacity_remaining_word} of work remaining"
+    end
   end
+
+  def capacity_remaining_word
+    project.capacity_remaining.to_business_days
+  end
+  private :capacity_remaining_word
+
+  def capacity_remaining_decimal
+    project.capacity_remaining / 8.0
+  end
+  private :capacity_remaining_decimal
+
+  def weekly_burn_rate
+    if project.weekly_burn_rate > 0
+      weekly_burn_rate_word
+    else
+      "no weekly burn rate"
+    end
+  end
+
+  def weekly_burn_rate_word
+    project.weekly_burn_rate.to_business_days
+  end
+  private :weekly_burn_rate_word
+
+  def weekly_burn_rate_decimal
+    project.weekly_burn_rate / 8.0
+  end
+  private :weekly_burn_rate_decimal
 
   def weekly_capacity_remaining
     if project.weekly_capacity_remaining > 0
-      project.weekly_capacity_remaining.to_business_days
+      weekly_capacity_remaining_word
     else
-      "No capacity"
+      "zero"
     end
   end
+
+  def weekly_capacity_remaining_word
+    project.weekly_capacity_remaining.to_business_days
+  end
+  private :weekly_capacity_remaining_word
+
+  def weekly_capacity_remaining_decimal
+    project.weekly_capacity_remaining / 8.0
+  end
+  private :weekly_capacity_remaining_decimal
 end
