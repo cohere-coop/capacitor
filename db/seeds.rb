603399bfe34083c30550a61d412a315b2b6ddbd3
Dicko Sow <s12.dsow@gmail.com>
2015-06-05 13:18:53 -0400

    should not seed to production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-if Rails.env.production?
-  if Account.count.zero?
-    Account.create!(email: "zee@zincma.de", name: "Zee Spencer", password: ENV["SEED_PASSWORD"])
-  else
-    Rails.logger.warn("Avoiding seeding an already seeded database")
-  end
-
-  if Project.count.zero?
-    Project.create!(name: "Capacitor", capacity: -1)
-  else
-    Rails.logger.warn("Avoiding seeding an already seeded database")
-  end
-else
+unless Rails.env.production?
   seeds = YAML.load_file(Rails.root.join("db", "seeds.yml"))
   accounts = seeds["accounts"].map do |seed|
     Account.create_with(seed).find_or_create_by(email: seed["email"])
