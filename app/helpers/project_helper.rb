8620d60a8e6e34a1b5ca1b68746d1215fa13994b
Zee Spencer <zee@zeespencer.com>
2015-06-09 15:00:50 -0700

    Default quantity of capacity to blank

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -2,7 +2,7 @@ require "capacity_converter"
 
 module ProjectHelper
   using CapacityConverter
-  AMOUNT_OPTIONS = [1, 2, 4, 6, 8].map { |i| [i.to_business_days, i] }
+  AMOUNT_OPTIONS = [1, 2, 4, 6, 8].map { |i| [i.to_business_days, i] }.unshift(nil)
 
   WORST = 1
   BAD = 2
