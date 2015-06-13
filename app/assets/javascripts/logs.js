2d92e39ce6bb9ac30d0dc97fac47dbb2de43146a
Anna Neyzberg <aneyzberg@gmail.com>
2015-06-12 19:38:59 -0700

    finished refactoring of log js

--- app/assets/javascripts/logs.js
+++ app/assets/javascripts/logs.js
@@ -1,28 +1,31 @@
-$(document).ready(function () {
-  enableDropdownForLogFilters();
-});
+(function(){
+    $(document).ready(function () {
+        enableDropdownForLogFilters();
+    });
 
-var enableDropdownForLogFilters = function() { 
-  $('.filters').each(function(index) {
-    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
-  });
-  $('.filters').on('click', 'li > a.filter-link', function(event) {
-    event.preventDefault();
-    if (isInactive(this)) {
-      showDropdownForLink(this);
+    var enableDropdownForLogFilters = function () {
+        $('.filters').on('click', 'li > a.filter-link', function (event) {
+            event.preventDefault();
+            if (isInactive(this)) {
+                showDropdownForLink(this);
+            }
+        });
     }
-  });
-}
 
-var isInactive = function(element) {
-  return !$(element).hasClass('is-active');
-}
+    var isInactive = function (element) {
+        return !$(element).hasClass('is-active');
+    }
 
-var showDropdownForLink = function(link) {
-  var filters = $(link).closest('.filters');
-  filters.find('.is-open').removeClass('is-open').hide();
-
-  $(link).next().toggleClass('is-open').toggle();
-  filters.find('.is-active').removeClass('is-active');
-  $(link).addClass('is-active');
-}
+    var showDropdownForLink = function (link) {
+        var $link = $(link)
+        var filters = $link.closest('.filters');
+        // closes currently open tab
+        filters.find('.is-open').removeClass('is-open').hide();
+        //find the content for the dropdown for this link and show it
+        $link.next().toggleClass('is-open').toggle();
+        //finds currently active filtered link and removes it
+        filters.find('.is-active').removeClass('is-active');
+        // makes link that was clicked active
+        $link.addClass('is-active');
+    }
+})();
