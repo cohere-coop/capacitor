2e1a54fd2058713903d8b9b5278bb6453858715b
Zee Spencer <zee@zeespencer.com>
2015-06-14 10:50:07 -0700

    Quality is now "How'd it go?"
    1. The order was backwards in the QUALITY_OPTIONS. It goes ["User facing value",
       "computer facing value"]
    2. We can use the actual emoji characters in our source code.
    3. Updated the tests to use the real emoji characters as well

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -4,12 +4,12 @@ module ProjectHelper
   using CapacityConverter
   AMOUNT_OPTIONS = [1, 2, 4, 6, 8].map { |i| [i.to_business_days, i] }.unshift(nil)
 
-  WORST = "\xF0\x9F\x98\xA9"
-  BAD = "\xF0\x9F\x98\xA5"
-  OKAY = "\xF0\x9F\x98\x9E"
-  GOOD = "\xF0\x9F\x98\x84"
-  BEST = "\xF0\x9F\x98\x86"
-  QUALITY_OPTIONS = [[nil], [1, WORST], [2, BAD], [3, OKAY], [4, GOOD], [5, BEST]]
+  WORST = "😡"
+  BAD = "😟"
+  OKAY = "😐"
+  GOOD = "😌"
+  BEST = "😄"
+  QUALITY_OPTIONS = [[nil], [WORST, 1], [BAD, 2], [OKAY, 3], [GOOD, 4], [BEST, 5]]
 
   def options_for_project_amount
     options_for_select(AMOUNT_OPTIONS)
