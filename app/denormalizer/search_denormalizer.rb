ea19fdd6e91cf47189241d4c028a178c47385adb
Zee Spencer <zee@zeespencer.com>
2015-06-14 11:23:36 -0700

    Un-remove the search denormalizer

---
+++ app/denormalizer/search_denormalizer.rb
@@ -0,0 +1,37 @@
+class SearchDenormalizer
+  DEFAULT_SCOPES = []
+  ALLOWED_SCOPES = ["recent", "weekly", "for_project", "billable", "order"]
+
+  def initialize(parameters)
+    @scopes = parameters[:scopes] || DEFAULT_SCOPES
+    @arguments = ALLOWED_SCOPES.inject({}) do |query, scope|
+      query.merge(scope => parameters[scope])
+    end
+  end
+
+  def scopes
+    @scopes.select do |scope|
+      ALLOWED_SCOPES.include?(scope)
+    end
+  end
+
+  def for_project
+    @arguments["for_project"]
+  end
+
+  def weekly
+    (@arguments["weekly"].to_i || 0).weeks
+  end
+
+  def recent
+    @arguments["recent"].to_i.days.ago
+  end
+
+  def order
+    @arguments["order"]
+  end
+
+  def billable
+    @arguments["billable"]
+  end
+end
