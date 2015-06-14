ca9a9e6c93bd4eccd46a842dd38bebead8c85aa0
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2015-06-13 22:16:35 -0400

    changed quality from numbers to emojis

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -4,11 +4,11 @@ module ProjectHelper
   using CapacityConverter
   AMOUNT_OPTIONS = [1, 2, 4, 6, 8].map { |i| [i.to_business_days, i] }.unshift(nil)
 
-  WORST = 1
-  BAD = 2
-  OKAY = 3
-  GOOD = 4
-  BEST = 5
+  WORST = "\xF0\x9F\x98\xA9"
+  BAD = "\xF0\x9F\x98\xA5"
+  OKAY = "\xF0\x9F\x98\x9E"
+  GOOD = "\xF0\x9F\x98\x84"
+  BEST = "\xF0\x9F\x98\x86"
   QUALITY_OPTIONS = [nil, WORST, BAD, OKAY, GOOD, BEST]
 
   def options_for_project_amount
