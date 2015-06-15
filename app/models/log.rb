a99b95c110e506888315a419bba4603877d4cb9a
Zee Spencer <zee@zeespencer.com>
2015-06-15 14:06:01 -0700

    Extract filter class from scope

--- app/models/log.rb
+++ app/models/log.rb
@@ -16,22 +16,6 @@ class Log < ActiveRecord::Base
   end
 
   def self.filter(conditions)
-    where_clauses = []
-    where_clauses.push(["worked_at >= ?", conditions[:after_date]]) if conditions[:after_date]
-    where_clauses.push(["worked_at <= ?", conditions[:before_date]]) if conditions[:before_date]
-    where_clauses.push(do_not_bill: false) if conditions[:billable]
-    where_clauses.push(project_id: conditions[:projects]) if conditions[:projects]
-    where_clauses.reduce(all) do |search_query, clause|
-      search_query.filter_clause(clause)
-    end
-  end
-
-  def self.filter_clause(clause)
-    if clause.respond_to?(:keys)
-      where(clause)
-    else
-      field, condition = clause
-      where(field, condition)
-    end
+    Filter.new(collection: all, conditions: conditions).results
   end
 end
