fce529fa054f44be299a77616864dbf810b072ea
Zee Spencer <zee@zeespencer.com>
2015-06-14 17:03:11 -0700

    User may look back months in time

--- app/decorators/logs_decorator.rb
+++ app/decorators/logs_decorator.rb
@@ -1,11 +1,23 @@
 class LogsDecorator < Draper::CollectionDecorator
-  DEFAULT_RECENT_RANGES = [7, 14, 21]
-  DEFAULT_ORDERS = [:worked_at, :quality, :amount]
-  def recents
-    DEFAULT_RECENT_RANGES
+  def ranges
+    [0, 1,2,3,4].map do |x|
+      {
+        after_date: x.weeks.ago.beginning_of_week.to_date,
+        before_date: x.weeks.ago.end_of_week.to_date,
+        label: label_for(x, "week")
+      }
+    end + [0,1,2,3].map do |x|
+      {
+        after_date: x.months.ago.beginning_of_month.to_date,
+        before_date: x.months.ago.end_of_month.to_date,
+        label: label_for(x, "month")
+      }
+    end
   end
 
-  def orders
-    DEFAULT_ORDERS
+  def label_for(qty, measure)
+    return "This #{measure}" if qty == 0
+    return "Last #{measure}" if qty == 1
+    "#{qty} #{measure.pluralize(qty)} ago"
   end
 end
