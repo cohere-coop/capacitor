390714701b17d637d52e0702b367d6d622b38c06
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2015-06-28 09:36:47 -0400

    added time zone logic to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_time_zone
+
 
   include SegmentRails
 
@@ -13,5 +15,9 @@ class ApplicationController < ActionController::Base
 
   def current_account
     devise_current_account.present? ? devise_current_account.decorate : nil
+  end
+
+  def set_time_zone
+    Time.zone = current_account.time_zone if current_account
   end
 end
