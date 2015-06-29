95352e384afe6d8e32e1abfab5028f52d4c375dc
Dicko Sow <s12.dsow@gmail.com>
2015-06-28 23:42:59 -0400

    cleaning up team controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,27 +1,46 @@
 class TeamsController < ApplicationController
+  before_action :setup_variables, only: [:new, :create]
+
   def new
-    @team = Team.new
     @teams_project = TeamsProject.new
-    @projects = Project.active
   end
 
   def create
-    @team = Team.new(team_params)
     @team.leader = current_account
     @team.save
 
     @teams_project = TeamsProject.new(teams_project_params)
-    @teams_project.team = @team
+    @teams_project.team_id = @team.id
     @teams_project.save
 
     if @teams_project.valid? && @team.valid?
       flash[:notice] = "#{@team.name} created successfully"
       redirect_to :root
     else
-      @projects = Project.active
+      # @projects = Project.active
       render :new
     end
   end
+
+  def load_team
+    if params[:team]
+      @team = Team.new(team_params)
+    else
+      @team = Team.new
+    end
+  end
+  private "load_team"
+
+  def load_projects
+    @projects = Project.active
+  end
+  private "load_projects"
+
+  def setup_variables
+    load_team
+    load_projects
+  end
+  private "setup_variables"
 
   def team_params
     params.require(:team).permit(:name)
