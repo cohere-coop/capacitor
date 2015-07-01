94373d4e61c94aeebb2d22c4112fa847680a754a
Dicko Sow <s12.dsow@gmail.com>
2015-06-30 23:53:48 -0400

    Trying to figure out how to create has many association for team and projects

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -6,5 +6,13 @@ FactoryGirl.define do
     capacity 15
     weekly_burn_rate 4
     billable true
+    factory :projects do
+      transient do
+        projects_count 5
+      end
+      after(:create) do |teams, evaluator|
+        create_list(:projects, evaluator.projects_count, teams: teams)
+      end
+    end
   end
 end
