357222b7c4493cbf1571d060a781dcde34a14bfa
Dicko Sow <s12.dsow@gmail.com>
2015-07-02 19:10:20 -0400

    Start beginning of week as sunday and made all time use application time

--- spec/features/deleting_logged_capacity_spec.rb
+++ spec/features/deleting_logged_capacity_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-feature "Logging time" do
+feature "Deleting log capacity" do
   include_context "account login"
   Given!(:log) { FactoryGirl.create(:recent_log, account: current_account).decorate }
   When { within("##{log.dom_id}") { click_link_or_button("Delete") } }
