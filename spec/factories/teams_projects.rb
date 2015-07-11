c315b6ff1baff6f4db45534441d710952bce80bf
Dicko Sow <s12.dsow@gmail.com>
2015-07-11 17:24:47 -0400

    User may assign teams from project new/edit pages

---
+++ spec/factories/teams_projects.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :teams_project do
+    team
+    project
+  end
+end
--- spec/features/edit_project_spec.rb
+++ spec/features/edit_project_spec.rb
@@ -4,15 +4,24 @@ feature "Edit projects" do
   Given!(:project) { FactoryGirl.create(:project, name: "Project A", capacity: 10) }
   Given!(:decorated_project) { project.decorate }
 
+  Given!(:team_1) { FactoryGirl.create(:team, name: "Team A") }
+  Given!(:team_2) { FactoryGirl.create(:team, name: "Team B") }
+
+  Given!(:assignment_1) { FactoryGirl.create(:teams_project, team: team_1, project: project) }
+
   include_context "account login"
 
   When { within("##{decorated_project.dom_id}") { click_link_or_button "Edit" } }
 
   context "successfully edits project" do
     When { fill_in "Capacity", with: 8 }
+    When { uncheck "Team A" }
+    When { check "Team B" }
     When { click_link_or_button "Save" }
 
     Then { expect(project.reload.capacity).to eql(8) }
+    Then { expect(project.teams).not_to include(team_1) }
+    Then { expect(project.teams).to include(team_2) }
   end
 
   context "unsuccessfully edits project" do
