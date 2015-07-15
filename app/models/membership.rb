d634b7549138d743db2c887bb6cb5df72ce00189
Dicko Sow <s12.dsow@gmail.com>
2015-07-15 14:33:17 -0400

    Add validation for level in membership table

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,6 +3,7 @@ class Membership < ActiveRecord::Base
   belongs_to :account
 
   validates :team, :account, presence: true
+  validates :level, inclusion: { in: %w(member leader) }
 
   def make_leader
     update_attributes(level: "leader")
