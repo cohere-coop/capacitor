dfb1ed9c65c9f84d2157fddf7d311aa3054c3e5d
Wimsy113 <krystyna.d.l.ewing@gmail.com>
2016-02-12 13:36:48 -0500

    Quality by week utilizes the last two months as default

--- app/models/log.rb
+++ app/models/log.rb
@@ -20,12 +20,11 @@ class Log < ActiveRecord::Base
     order(worked_at: :desc).where(worked_at: week.beginning_of_week..week.end_of_week)
   end
 
-  scope :two_months_ago, ->(months_ago = 2) do
-      where(created_at: 2.months_ago)
+  scope :up_to_two_months_ago, ->(months_ago = 2) do
+    recent(2.months.ago)
   end
 
   def self.filter(conditions)
     Filter.new(collection: all, conditions: conditions).results
-    project.logs.up_to_two_months_ago
   end
 end
