34a13cb8029250b48dc0362191647ad525d2e21a
Wimsy113 <krystyna.d.l.ewing@gmail.com>
2016-02-28 17:19:11 -0500

    set up route for check ins page

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,4 +1,7 @@
 class CheckInsController < ApplicationController
+  # before_action :setup_variables, only: [:new, :create]
+  def index
+  end
 
   def new
     @check_in = CheckIn.new(account: current_account)
