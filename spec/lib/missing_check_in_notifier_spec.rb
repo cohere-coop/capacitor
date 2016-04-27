3e0365617b058031c0659821a8d30cde6b90811d
Ubuntu <ubuntu@ip-172-31-1-99.us-west-2.compute.internal>
2016-04-27 17:49:38 +0000

    Verify we don't email people without check in feature

--- spec/lib/missing_check_in_notifier_spec.rb
+++ spec/lib/missing_check_in_notifier_spec.rb
@@ -4,18 +4,44 @@ require "rails_helper"
 describe MissingCheckInNotifier do
   describe ".notify" do
     include Rails.application.routes.url_helpers
-    When { default_url_options[:host] = "example.com" }
+    Given { default_url_options[:host] = "example.com" }
+    Given { ActionMailer::Base.deliveries.clear }
     Given!(:account_without_check_in_yesterday) { FactoryGirl.create(:account) }
     Given!(:account_who_checked_in_yesterday) { FactoryGirl.create(:account_who_checked_in_yesterday) }
-    Given { account_without_check_in_yesterday.features.check_in = true }
-    Given { account_who_checked_in_yesterday.features.check_in = true }
-    Given(:sent_email) { ActionMailer::Base.deliveries.last }
 
-    When { described_class.notify }
+    let(:sent_email) { ActionMailer::Base.deliveries.last }
 
-    Then { ActionMailer::Base.deliveries.length == 1 }
-    Then { sent_email.to == [account_without_check_in_yesterday.email] }
-    Then { sent_email.subject == "Let's check in your activities from #{1.day.ago.to_date.to_s(:long_ordinal)}" }
-    Then { sent_email.body.encoded.include? new_check_in_url }
+    context "when the accounts have check in enabled" do
+      Given do
+        account_without_check_in_yesterday.features.check_in = true
+        account_without_check_in_yesterday.save
+      end
+      Given do
+        account_who_checked_in_yesterday.features.check_in = true
+        account_who_checked_in_yesterday.save
+      end
+
+      When { described_class.notify }
+
+      Then { ActionMailer::Base.deliveries.length == 1 }
+      Then { sent_email.to == [account_without_check_in_yesterday.email] }
+      Then { sent_email.subject == "Let's check in your activities from #{1.day.ago.to_date.to_s(:long_ordinal)}" }
+      Then { sent_email.body.encoded.include? new_check_in_url }
+    end
+
+    context "when the accounts do not have check in enabled" do
+      Given do
+        account_without_check_in_yesterday.features.check_in = false
+        account_without_check_in_yesterday.save
+      end
+      Given do
+        account_who_checked_in_yesterday.features.check_in = false
+        account_who_checked_in_yesterday.save
+      end
+
+      When { described_class.notify }
+
+      Then { ActionMailer::Base.deliveries.length == 0 }
+    end
   end
 end
