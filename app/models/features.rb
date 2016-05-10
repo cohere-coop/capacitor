de95cf339269073c3e58de930160562eedcf548a
Wimsy113 <krystyna.d.l.ewing@gmail.com>
2016-05-10 04:04:55 -0400

    check in enabled by default

--- app/models/features.rb
+++ app/models/features.rb
@@ -1,7 +1,7 @@
 # Co-erces an HStore object of feature flags into a ruby model
 class Features
   include Virtus.model
-  attribute :check_in, Boolean, default: false
+  attribute :check_in, Boolean, default: true
 
   def self.dump(features)
     features.to_hash
