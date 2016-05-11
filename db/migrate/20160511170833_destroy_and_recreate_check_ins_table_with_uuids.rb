718cd0f77596229b19cfcc583bb3b2885f0b5116
Zee <zee@zeespencer.com>
2016-05-11 10:18:44 -0700

    Use uuids for check_ins id account_id fields (#222)

---
+++ db/migrate/20160511170833_destroy_and_recreate_check_ins_table_with_uuids.rb
@@ -0,0 +1,14 @@
+class DestroyAndRecreateCheckInsTableWithUuids < ActiveRecord::Migration
+  def change
+    drop_table :check_ins
+    create_table :check_ins, id: :uuid  do |t|
+      t.references :account, null: false, type: :uuid
+      t.date :worked_at
+      t.timestamps null: false
+    end
+    remove_column :logs, :check_in_id
+    change_table :logs do |t|
+      t.references :check_in, type: :uuid
+    end
+  end
+end
