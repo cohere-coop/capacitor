6e1b21cbae0b5273ec5109b50a980c445021608c
Zee Spencer <zee@zincma.de>
2016-05-14 13:08:32 -0700

    Disable billing time features unless account has them enabled

--- app/models/features.rb
+++ app/models/features.rb
@@ -1,7 +1,9 @@
 # Co-erces an HStore object of feature flags into a ruby model
 class Features
   include Virtus.model
+
   attribute :check_in, Boolean, default: true
+  attribute :bills_time, Boolean, default: false
 
   def self.dump(features)
     features.to_hash
--- spec/features/reviewing_recently_logged_capacity_spec.rb
+++ spec/features/reviewing_recently_logged_capacity_spec.rb
@@ -7,6 +7,5 @@ feature "Reviewing logged capacity" do
 
   Then { within("##{recent_log.dom_id}") { expect(page).to have_content(recent_log.worked_at) } }
   Then { within("##{recent_log.dom_id}") { expect(page).to have_content(recent_log.activity.name) } }
-  Then { within("##{recent_log.dom_id}") { expect(page).to have_content(recent_log.billable?) } }
   Then { expect(page).not_to have_selector("##{old_log.dom_id}") }
 end
