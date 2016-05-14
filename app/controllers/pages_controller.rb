137a6d7eae8ff142ae1be7988b9f9e6f25ca8e8c
Zee Spencer <zee@zincma.de>
2016-05-14 12:54:45 -0700

    Users may still edit and see the activities they have on their account

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,16 @@
 # Controller for the Dashboard view
 class PagesController < ApplicationController
   before_action :authenticate_account!
-
+  decorates_assigned :check_ins
+  decorates_assigned :activities
   def dashboard
     @activities = current_account.active_activities
+
     if current_account.features.check_in?
+      @check_ins = CheckIns::Account.new(current_account).recent_check_ins
       render "check_ins/dashboard"
     else
       render :dashboard
     end
-  end
-
-  helper_method def check_ins
-    @check_ins ||= CheckIns::Account.new(current_account).recent_check_ins.map(&:decorate)
   end
 end
