bf362449770d87d0710d5f0b92e8749d1b13c60c
Krystyna Ewing <Krystyna.d.l.ewing@gmail.com>
2016-05-18 14:10:37 -0400

    Capacitor#261 -Time zone can be set and edited for accounts (#264)

--- app/controllers/account/registrations_controller.rb
+++ app/controllers/account/registrations_controller.rb
@@ -49,12 +49,12 @@ class Account
 
     # You can put the params you want to permit in the empty array.
     def configure_sign_up_params
-      devise_parameter_sanitizer.for(:sign_up) << :name
+      devise_parameter_sanitizer.for(:sign_up) << [:name, :time_zone]
     end
 
     # You can put the params you want to permit in the empty array.
     def configure_account_update_params
-      devise_parameter_sanitizer.for(:account_update) << :name
+      devise_parameter_sanitizer.for(:account_update) << [:name, :time_zone]
     end
 
     # The path used after sign up.
