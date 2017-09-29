072d4ddff81acb174cb93f56705012fcb0ffbe42
Zee Spencer <zee@zincma.de>
2017-09-29 13:07:50 -0700

    Production hadn't ran this migration

--- db/migrate/20170812043001_remove_null_constraint_on_account_password.rb
+++ db/migrate/20170812043001_remove_null_constraint_on_account_password.rb
@@ -1,4 +1,4 @@
-class RemoveNullConstraintOnAccountPassword < ActiveRecord::Migration
+class RemoveNullConstraintOnAccountPassword < ActiveRecord::Migration[4.2]
   def change
     change_column :accounts, :encrypted_password, :string,  null: true
   end
