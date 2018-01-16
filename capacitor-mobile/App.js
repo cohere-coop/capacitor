c7de5ad4e5408a1fbcd5d3950567198d9ef92f31
Zee Spencer <zee@wecohere.com>
2018-01-15 18:17:58 -0800

    Oauth actually works

--- capacitor-mobile/App.js
+++ capacitor-mobile/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { AuthSession } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    result: null
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>I WROTE A THING</Text>
-        <Text>LOOK AT ME COMPUTERING</Text>
-        <Text>LIKE A BOSS</Text>
+        <Text>Helllllllooooo</Text>
+        <Button title="Login" onPress={this._handlePressAsync }/>
+        {this.state.result ? (
+          <Text>{JSON.stringify(this.state.result)}</Text>
+        ): null}
       </View>
     );
+  }
+
+  _handlePressAsync = async () => {
+    let redirectUrl = AuthSession.getRedirectUrl();
+    let result = await AuthSession.startAsync({
+      authUrl: `https://cd952c18.ngrok.io/oauth/authorize?response_type=code` +
+               `&client_id=THIS_IS_A_SECRET_AND_I_DONT_KNOW_HOW_TO_POPULATE_SECRETS_SAFELY_YET` +
+               `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+    });
+    this.setState({ result });
   }
 }
 
