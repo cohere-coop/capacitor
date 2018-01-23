9764609eca8b2920e11d874220dfe967658a876a
Zee Spencer <zee@wecohere.com>
2018-01-22 19:04:32 -0800

    Fix the rubocop error

--- app/controllers/oauth/applications_controller.rb
+++ app/controllers/oauth/applications_controller.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
-# Enforces ownership of applications
-class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
-  before_action :authenticate_account!
+module Oauth
+  # Enforces ownership of applications
+  class ApplicationsController < Doorkeeper::ApplicationsController
+    before_action :authenticate_account!
 
-  def index
-    @applications = current_account.oauth_applications
-  end
-
-  def create
-    @application = current_account.oauth_applications.new(application_params)
-
-    if @application.save
-      flash[:notice] = I18n.t(:notice, scope: %i[doorkeeper flash applications create])
-    else
-      render :new
+    def index
+      @applications = current_account.oauth_applications
     end
-  end
 
-  def set_application
-    @application = current_account.oauth_applications.find(params[:id])
+    def create
+      @application = current_account.oauth_applications.new(application_params)
+
+      if @application.save
+        flash[:notice] = I18n.t(:notice, scope: %i[doorkeeper flash applications create])
+      else
+        render :new
+      end
+    end
+
+    def set_application
+      @application = current_account.oauth_applications.find(params[:id])
+    end
   end
 end
